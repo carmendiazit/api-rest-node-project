878f9a5f8a7fa035d01f4626100a86279edce463
carmecha <carmendiazit@gmail.com>
2025-07-17 13:52:47 -0300

    update deploy

--- index.js
+++ index.js
@@ -26,13 +26,9 @@ app.use((req, res, next )=> {
   res.status(404).json({error: "Not found"});   
 
 });
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 3001;
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`));
-}
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
-// Export the app for Vercel
 export default app;
 
